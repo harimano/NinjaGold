0abdc226b14bf07415a5e78fc81d7007bc7e273c
haythamodeh <HOdehLion@MacBook-Pro.local>
2019-03-18 14:08:09 -0700

    added the code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'NinjaGold';
+  title = 'Ninja Gold';
+  gold: number = 0;
+  log = [];
+
+
+  addGold(building: String) {
+    if(building === "farm"){
+      let random = Math.floor((Math.random() * 4) + 2);
+      this.gold += random;
+      let buildingLog = `You earned ${random} gold at the ${building}!`;
+      this.log.push(buildingLog);
+    }
+    if(building === "cave"){
+      let random = Math.floor((Math.random() * 9) + 5);
+      this.gold += random;
+      let buildingLog = `You earned ${random} gold at the ${building}!`;
+      this.log.push(buildingLog);
+    }
+    if(building === "house"){
+      let random = Math.floor((Math.random() * 14) + 7);
+      this.gold += random;
+      let buildingLog = `You earned ${random} gold at the ${building}!`;
+      this.log.push(buildingLog);
+    }
+    if(building === "casino"){
+      let random = Math.floor((Math.random() * 200) - 100);
+      this.gold += random;
+      let buildingLog
+      if(random > 0){
+        buildingLog = `You earned ${random} gold at the ${building}!`;
+      }
+      else {
+        buildingLog = `You lost ${random} gold at the ${building}!`;
+      }
+      this.log.push(buildingLog);
+    }
+    
+  }
+
+
 }
